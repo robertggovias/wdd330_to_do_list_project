7111629f80de625a3ced068d35723212f2b85240
robertggovias <goviasuarez@gmail.com>
2024-12-08 03:55:16 -0300

    first-try-recipe

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,18 @@
-export function createElement(type, props = {}, children = []) {
-    const element = document.createElement(type);
-  
-    // props: {textContent: "Hello world!", id: "header1", "data-productId": 123, ...}
-    Object.entries(props).forEach(([key, value]) => {
-      if(~key.indexOf('-')) {
-        element.setAttribute(key, value); // data attributes
-      } else {
-        element[key] = value;
-      }
-    });
-  
-    children.forEach((child) => {
-      element.appendChild(child);
-    });
-  
-    return element;
-  }
+export function createElement(tipo, textos = {}, filho = []) {
+  const pepito = document.createElement(tipo);
+
+  // textos: {textContent: "Hello world!", id: "header1", "data-productId": 123, ...}
+  Object.entries(textos).forEach(([nombre, valor]) => {
+    if(~nombre.indexOf('-')) {
+      pepito.setAttribute(nombre, valor); // data attributes
+    } else {
+      pepito[nombre] = valor;
+    }
+  });
+
+  filho.forEach((hijo) => {
+    pepito.appendChild(hijo);
+  });
+
+  return pepito;
+}
